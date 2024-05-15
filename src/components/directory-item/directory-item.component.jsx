3a02cc508cdfae424c4c9bd700354c43aa96ba9e
George <Georgeheniin@hotmail.com>
2024-05-15 13:23:20 -0400

    added links to home grid

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,29 @@
 import "./directory-item.styles.scss"; 
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({category}) => {
-    const {title, imageUrl} = category
-    return (
-      <div className="directory-item-container">
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
-        />
-        <div className="directory-body-container">
-          <h2>{title}</h2>
-          <p> Shop Now </p>
-        </div>
+  const { title, imageUrl, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(route);
+    console.log(route);
+  }
+
+  return (
+    <div className="directory-item-container" onClick={onNavigateHandler}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="directory-body-container">
+        <h2>{title.toUpperCase()}</h2>
+        <p> Shop Now </p>
       </div>
-    );
+    </div>
+  );
 };
 
 export default DirectoryItem;
