3fed81754e9b1d4d2ece5c77110fa276d07b40ed
George <Georgeheniin@hotmail.com>
2024-05-21 09:41:12 -0400

    setting up redux store

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -13,8 +13,6 @@ export const USER_ACTION_TYPES = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
