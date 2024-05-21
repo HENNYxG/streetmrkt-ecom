4cf72216c923dee49eb4a73b72e06ccefea732f7
George <Georgeheniin@hotmail.com>
2024-05-21 10:17:09 -0400

    converting user context into redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
+import { useSelector } from "react-redux";
+
+import { selectCurrentUser } from '../../store/user/user.selector'
 import "./navigation.styles.scss";
 import SMLogo from "../../assets/streetmrkt-logo.png";
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CardDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-   const { isCartOpen } = useContext(CartContext);
- // console.log(currentUser);
+  const currentUser = useSelector(selectCurrentUser);
+
+  const { isCartOpen } = useContext(CartContext);
+
 
   return (
     <Fragment>
