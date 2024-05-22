00da738bf32796b2dadf37a2f09f1e8e882e456a
George <Georgeheniin@hotmail.com>
2024-05-22 19:53:42 -0400

    added redux thunk to fetch data and added loader state visual component

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,4 +1,27 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
 
-export const setCategories = (categoriesArray) =>
-    ({ type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES, payload: categoriesArray });
+
+export const fetchCategoriesStart = () => 
+    ({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START })
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+    ({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, payload: categoriesArray });
+
+export const fetchCategoriesFailed = (error) =>
+({ type: CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, payload: error });
+
+//thunk action. (a function that returns a function that gets dispatch)
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart);
+    console.log('changed to true')
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments("categories");
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+        console.log("changed to false");
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 // import { loggerMiddleware } from '../middleware/logger'; (this is more accurate for rerenders)
+import {thunk} from 'redux-thunk'
 
 import { rootReducer } from './root-reducer';
 
@@ -10,12 +11,12 @@ import { rootReducer } from './root-reducer';
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
